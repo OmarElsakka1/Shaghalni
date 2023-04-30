a1126c06317510180c634494b0b5df686a20d6f2
Omar El-Sakka <68303138+OmarElsakka1@users.noreply.github.com>
2023-04-30 02:33:44 +0200

    Update models.py

--- website/models.py
+++ website/models.py
@@ -16,12 +16,15 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
-    first_name = db.Column(db.String(150))
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
+    job_des = db.column(db.String(2000))
+    category = db.column(db.String(150))
     notes = db.relationship('Note')
 
 class Job(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     job_name = db.Column(db.String(150))
-    job_description = db.Column(db.String(300))
+    job_description = db.Column(db.String(1000))
     job_date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
