e2434a1cbfe05de84cb9d1659accd009695d50ac
Omar El-Sakka <68303138+OmarElsakka1@users.noreply.github.com>
2023-05-24 00:39:49 +0300

    Update and rename Admin_Testing.py to admin_testing.py

--- admin_testing.py
+++ admin_testing.py
@@ -1,7 +1,6 @@
-import pytest
-from Functions import adapter,login_action_testing
+from functions import Adapter,LoginActionTesting
 
-base_url = "https://python-test-dev-ezzat.azurewebsites.net/"
+BASE_URL = "https://python-test-dev-ezzat.azurewebsites.net" # "http://localhost" #
 
 def test_add_admin():
     email = "admin2@Shaghlni.com"
@@ -14,39 +13,39 @@ def test_add_admin():
     fail_fistlength_msg = "First name must be at least"
     fail_lastlength_msg = "Last name must be at least"
 
-    path = f"{base_url}/admin/adminlogin"
-    pathform = f"{base_url}/admin/adminadd_admin"
+    path = f"{BASE_URL}/admin/adminlogin"
+    pathform = f"{BASE_URL}/admin/adminadd_admin"
 
     # Testing Good account
-    assert adapter(path,pathform).do_login_add_admin(email = email, password = password,
+    assert Adapter(path,pathform).do_login_add_admin(email = email, password = password,
                                                     msg= succes_msg,
                                                     newemail="admin3@Shaghlni.com",
                                                     firstname="Mahrous",
                                                     lastname="Nour",
                                                     pass1 = "1234567", pass2 = "1234567") == True
     ## Testing not typical passwords formatting
-    assert adapter(path,pathform).do_login_add_admin(email = email, password = password,
+    assert Adapter(path,pathform).do_login_add_admin(email = email, password = password,
                                                     msg= fail_passmatch_msg,
                                                     newemail="admin4@Shaghlni.com",
                                                     firstname="Mahrous",
                                                     lastname="Nour",
                                                     pass1 = "123", pass2 = "1234567") == True
     # Testing Small Password formatting
-    assert adapter(path,pathform).do_login_add_admin(email = email, password = password,
+    assert Adapter(path,pathform).do_login_add_admin(email = email, password = password,
                                                     msg= fail_pass_length_msg,
                                                     newemail="admin5@Shaghlni.com",
                                                     firstname="Mahrous",
                                                     lastname="Nour",
                                                     pass1 = "123", pass2 = "123") == True
     # Testing Small First name formatting
-    assert adapter(path,pathform).do_login_add_admin(email = email, password = password,
+    assert Adapter(path,pathform).do_login_add_admin(email = email, password = password,
                                                     msg= fail_fistlength_msg,
                                                     newemail="admin6@Shaghlni.com",
                                                     firstname="M",
                                                     lastname="Nour",
                                                     pass1 = "1234567", pass2 = "1234567") == True
     # Testing Small Last name formatting
-    assert adapter(path,pathform).do_login_add_admin(email = email, password = password,
+    assert Adapter(path,pathform).do_login_add_admin(email = email, password = password,
                                                     msg= fail_lastlength_msg,
                                                     newemail="admin7@Shaghlni.com",
                                                     firstname="Mahrous",
@@ -54,7 +53,7 @@ def test_add_admin():
                                                     pass1 = "1234567", pass2 = "1234567") == True
     
     # Testing good account
-    assert adapter(path,pathform).do_login_add_admin(email = email, password = password,
+    assert Adapter(path,pathform).do_login_add_admin(email = email, password = password,
                                                     msg= succes_msg,
                                                     newemail="admin8@Shaghlni.com",
                                                     firstname="Omar",
@@ -64,7 +63,7 @@ def test_add_admin():
 
 
     # Testing good account
-    assert adapter(path,pathform).do_login_add_admin(email = email, password = password,
+    assert Adapter(path,pathform).do_login_add_admin(email = email, password = password,
                                                     msg= succes_msg,
                                                     newemail="admin9@Shaghlni.com",
                                                     firstname="Hanan",
@@ -72,7 +71,7 @@ def test_add_admin():
                                                     pass1 = "sdfghjk", pass2 = "sdfghjk") == True
     
     # Testing sign up with alread existing account
-    assert adapter(path,pathform).do_login_add_admin(email = email, password = password,
+    assert Adapter(path,pathform).do_login_add_admin(email = email, password = password,
                                                     msg= fail_emailexist_msg,
                                                     newemail="admin9@Shaghlni.com",
                                                     firstname="Mahrous",
@@ -80,7 +79,7 @@ def test_add_admin():
                                                     pass1 = "sdfghjk", pass2 = "sdfghjk") == True
 
     # Testing good account
-    assert adapter(path,pathform).do_login_add_admin(email = email, password = password,
+    assert Adapter(path,pathform).do_login_add_admin(email = email, password = password,
                                                     msg= succes_msg,
                                                     newemail="admin10@Shaghlni.com",
                                                     firstname="abdelhamed",
@@ -95,16 +94,16 @@ def test_change_password():
     success_msg = "Password Changed Successfully!\n×"
     page = "adminchangepassword"
     button = "admin_chgpass_submit"
-    path = f"{base_url}/admin/adminlogin"
+    path = f"{BASE_URL}/admin/adminlogin"
 
-    assert login_action_testing(path,
-                                     email, password).do_change_Password_action(password,"sdfghjk",success_msg,page, button) == True
-    assert login_action_testing(path,
-                                     email, "sdfghjk").do_change_Password_action("sdfghjk", password, success_msg,page, button) == True
-    assert login_action_testing(path,
-                                     email, password).do_change_Password_action(password,"1234",success_msg,page, button) == False
-    assert login_action_testing(path,
-                                     email, password).do_change_Password_action("1234","sdfghjk",success_msg,page, button) == False
+    assert LoginActionTesting(path,
+                                     email, password).do_change_password_action(password,"sdfghjk",success_msg,page, button) == True
+    assert LoginActionTesting(path,
+                                     email, "sdfghjk").do_change_password_action("sdfghjk", password, success_msg,page, button) == True
+    assert LoginActionTesting(path,
+                                     email, password).do_change_password_action(password,"1234",success_msg,page, button) == False
+    assert LoginActionTesting(path,
+                                     email, password).do_change_password_action("1234","sdfghjk",success_msg,page, button) == False
 
 
 
