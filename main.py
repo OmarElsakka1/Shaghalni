807c39f08994d1afc3c6129dbfb99e4bc11b5948
EzzatEsam <s-ezzat.eisawy@zewailcity.edu.eg>
2023-05-24 05:22:35 +0300

    .

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from website import create_app
+import os
 
 app = create_app()
-print("Start")
+
 if __name__ == '__main__':
-    app.run(debug=True , host = '0.0.0.0' , port = 80)
+    if os.environ.get('FLASK_ENV') == 'production' or os.path.exists('/.dockerenv'):
+        app.run(host='0.0.0.0', port=80)
+    else:
+        app.run(debug=True, host='0.0.0.0', port=80)
